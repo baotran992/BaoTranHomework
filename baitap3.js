a4460713781f3a765a3f00ddd75e07c33038edaf
Admin <admin@Admins-MacBook-Pro.local>
2023-09-17 23:47:39 +0700

    da xong btvn buoi 3

---
+++ baitap3.js
@@ -0,0 +1,46 @@
+// bai 1
+var height = 150;
+
+// bai 2
+//day la comment mot dong
+/**day la comment
+ * nhieu
+ * dong */
+
+// bai 3
+console.log ("chieu cao cua toi la" , height);
+
+// bai 4
+let weight = 20
+let nextWeight = weight - 3;
+console.log(nextWeight);
+
+// bai 5
+let a = 1
+let b = -1
+let c = a + b
+let d = a - b
+console.log(a , b , c, d );
+
+// bai 6
+var aa = 1;
+var bb = ++aa * aa--
+console.log(bb);
+
+// bai 7
+let fullName;
+let firstName = "tran";
+let lastName = "ha";
+fullName = firstName + lastName
+console.log(fullName);
+
+// bai 8
+let a1 = 1;
+let b1 = -1;
+let c1 = 0;
+let d1 = 0;
+
+let e = a1 <= b1;
+let f = c1 === d1;
+let g = a1 >= c1;
+console.log(e,f,g);
